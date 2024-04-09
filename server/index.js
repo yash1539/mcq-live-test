d89625aa6c547c0bf69c0e2e7e6b2e0402061894
Raramuri <raramuri@Raramuris-MacBook-Pro-96.local>
2024-04-09 13:30:05 +0530

    removed cora to test

--- server/index.js
+++ server/index.js
@@ -12,10 +12,10 @@ app.use(cors());
 
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
-  },
+  // cors: {
+  //   origin: "http://localhost:3000",
+  //   methods: ["GET", "POST"],
+  // },
 });
 
 app.get("/", (req, res) => {
